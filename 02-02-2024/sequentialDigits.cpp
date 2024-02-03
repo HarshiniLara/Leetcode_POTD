d03fdba5d4cc6bf9a08fd56fc03353efc8e887a3
Harshini Lara A K <harshukuberdevi@gmail.com>
2024-02-03 23:11:44 +0530

    Update sequentialDigits.cpp

--- 02-02-2024/sequentialDigits.cpp
+++ 02-02-2024/sequentialDigits.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> a;
-
-        for (int i = 1; i <= 9; ++i) {
-            int num = i;
-            int nextDigit = i + 1;
-
-            while (num <= high && nextDigit <= 9) {
-                num = num * 10 + nextDigit;
-                if (low <= num && num <= high) a.push_back(num);
-                ++nextDigit;
+        vector<int> res;
+        for(int i=1; i<10; i++) {
+            int temp = i, j = i+1;
+            while(temp<=high && j<10) {
+                temp = temp*10+j;
+                if(temp>=low && temp<=high) 
+                    res.push_back(temp);
+                j++;
             }
         }
-
-        sort(a.begin(), a.end());
-        return a;
+        sort(res.begin(), res.end());
+        return res;
     }
 };
