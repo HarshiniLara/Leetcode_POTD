5d7c5bd78162bd90fd235f864602c9aab1c30142
Harshini Lara A K <harshukuberdevi@gmail.com>
2024-03-05 20:23:15 +0530

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ 05-03-2024/minimumLength.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int l = 0, r = s.size()-1;
+        while(l<r && s[l]==s[r]) {
+            char ch = s[l];
+            while(s[l]==ch && l<r)  l++;
+            while(s[r]==ch && l<=r) r--;
+        } 
+        return r-l+1;
+    }
+};
